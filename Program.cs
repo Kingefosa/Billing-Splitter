f6b42204cf3d4be0967a614fb2d048eb90d9045d
Diego <djin072@uottawa.ca>
2018-02-06 11:21:18 -0500

    Initial commits

---
+++ Program.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+
+namespace ConsoleApp1
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            if (args.Length != 1) {
+                Console.WriteLine("Please only give the input filename as first paramters");
+                return;
+            }
+
+            FileStream fileStream;
+
+            try
+            {
+                fileStream = new FileStream(args[0], FileMode.Open, FileAccess.Read);
+            }
+            catch (FileNotFoundException e) {
+                Console.WriteLine(args[0] + " is not found, please give full path of the input file.");
+                return;
+            }
+
+            StreamReader reader = new StreamReader(fileStream);
+            String line = "";
+            TripDB tripDB = new TripDB();
+            int lastTripId = 0;
+            int lastPayerId = 0;
+            try
+            {
+                while ((line = reader.ReadLine()) != null)
+                {
+                    int numOfPeople = Int32.Parse(line);
+                    if (numOfPeople == 0)
+                        break;
+                    lastTripId = tripDB.addTrip(numOfPeople);
+
+                    for (int i = 0; i < numOfPeople; i++) {
+                        line = reader.ReadLine();
+                        int numOfPay = Int32.Parse(line);
+                        lastPayerId = tripDB.addPayer(lastTripId, i, numOfPay);
+
+                        for (int j = 0; j < numOfPay; j++) {
+                            line = reader.ReadLine();
+                            double price = Double.Parse(line);
+                            tripDB.addPrice(lastPayerId, j, price);
+                        }
+                    }
+                }
+
+            } catch (Exception e) {
+                Console.WriteLine("Input format is wrong.");
+                return;
+            }
+            reader.Close();
+            fileStream.Close();
+
+            fileStream = new FileStream(args[0] + ".out", FileMode.OpenOrCreate, FileAccess.Write);
+            StreamWriter writer = new StreamWriter(fileStream);
+            for (int i = 0; i < tripDB.getNoOfTrip(); i++) {
+                double[] remains = tripDB.getPayersRemain(i);
+                for (int j = 0; j < remains.Length; j++)
+                {
+                    String price = "$" + Math.Abs(remains[j]);
+                    if (remains[j] < 0) {
+                        price = "(" + price + ")";
+                    }
+                    writer.WriteLine(price);
+                }
+                writer.WriteLine();
+            }
+
+            writer.Close();
+            fileStream.Close();
+        }
+    }
+
+    public class TripDB {
+        private Dictionary<int, int[]> tripDict;
+        private Dictionary<int, double[]> payerDict;
+        private int tripId;
+        private int payerId;
+        public TripDB() {
+            tripDict = new Dictionary<int, int[]>();
+            payerDict = new Dictionary<int, double[]>();
+            tripId = 0;
+            payerId = 0;
+        }
+
+        public int addTrip(int n) {
+            int[] payerArray = new int[n];
+            tripDict.Add(tripId, payerArray);
+            tripId++;
+            return tripId - 1;
+        }
+
+        public int addPayer(int trip_id, int index, int noOfPay) {
+            //Create price array for this payer.
+            double[] priceArray = new double[noOfPay];
+            payerDict.Add(payerId, priceArray);
+
+            //Match the trip_id for this payer.
+            int[] payerArray = tripDict[trip_id];
+            payerArray[index] = payerId;
+
+            payerId++;
+            return payerId - 1;
+        }
+
+        public void addPrice(int payer_id, int index, double price) {
+            double[] priceArray = payerDict[payer_id];
+            priceArray[index] = price;
+        }
+
+        public int getNoOfTrip() {
+            return tripId;
+        }
+
+        public int getNoOfPayer() {
+            return payerId;
+        }
+
+        public double[] getPriceArray(int payer_id) {
+            return payerDict[payer_id];
+        }
+
+        public int[] getPayerArray(int trip_id) {
+            return tripDict[trip_id];
+        }
+
+        public double getTripAverage(int trip_id) {
+            int[] payerArray = getPayerArray(trip_id);
+            double total = 0.0;
+            for (int i = 0; i < payerArray.Length; i++) {
+                double[] priceArray = payerDict[payerArray[i]];
+                for (int j = 0; j < priceArray.Length; j++) {
+                    total = total + priceArray[j];
+                }
+            }
+            double noOfPayer = payerArray.Length;
+            double average = Math.Round(total / noOfPayer, 2);
+            return average;
+        }
+
+        public double[] getPayersRemain(int trip_id) {
+            int[] payerArray = getPayerArray(trip_id);
+            double[] remains = new double[payerArray.Length];
+            double average = getTripAverage(trip_id);
+            for (int i = 0; i < payerArray.Length; i++)
+            {
+                double total = 0.0;
+                double[] priceArray = payerDict[payerArray[i]];
+                for (int j = 0; j < priceArray.Length; j++) {
+                    total = total + priceArray[j];
+                }
+                remains[i] = Math.Round(average - total, 2);
+            }
+            return remains;
+        }
+    }
+}
---
+++ TripDBTests.cs
@@ -0,0 +1,124 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ConsoleApp1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1.Tests
+{
+    [TestClass()]
+    public class TripDBTests
+    {
+        [TestMethod()]
+        public void TripDBTest()
+        {
+            TripDB tripDB = new TripDB();
+            int p = tripDB.getNoOfPayer();
+            int n = tripDB.getNoOfTrip();
+            Assert.AreEqual(p, 0);
+            Assert.AreEqual(n, 0);
+        }
+
+        [TestMethod()]
+        public void addTripTest()
+        {
+            TripDB tripDB = new TripDB();
+            int before = tripDB.getNoOfTrip();
+            int tripID= tripDB.addTrip(3);
+            int after = tripDB.getNoOfTrip();
+            Assert.AreEqual(before + 1, after);
+            Assert.AreEqual(tripID, before);
+        }
+
+        [TestMethod()]
+        public void addPayerTest()
+        {
+            TripDB tripDB = new TripDB();
+            int tripID = tripDB.addTrip(2);
+            int payerId1= tripDB.addPayer(tripID, 0, 1);
+            int payerId2 = tripDB.addPayer(tripID, 1, 1);
+            Assert.AreEqual(payerId1 + 1, payerId2);
+        }
+
+        [TestMethod()]
+        public void getNoOfTripTest()
+        {
+            TripDB tripDB = new TripDB();
+            int tripID = tripDB.addTrip(3);
+            int n = tripDB.getNoOfTrip();
+            Assert.AreEqual(n , 1);
+        }
+
+        [TestMethod()]
+        public void getNoOfPayerTest()
+        {
+            TripDB tripDB = new TripDB();
+            int tripID = tripDB.addTrip(3);
+            int payerId1 = tripDB.addPayer(tripID, 0, 1);
+            int payerId2 = tripDB.addPayer(tripID, 1, 1);
+            int payerId3 = tripDB.addPayer(tripID, 2, 1);
+            int p = tripDB.getNoOfPayer();
+            Assert.AreEqual(p, 3);
+        }
+
+        [TestMethod()]
+        public void getPriceArrayTest()
+        {
+            TripDB tripDB = new TripDB();
+            int tripID = tripDB.addTrip(1);
+            int payerId1 = tripDB.addPayer(tripID, 0, 3);
+            tripDB.addPrice(payerId1, 0, 1.0);
+            tripDB.addPrice(payerId1, 1, 2.0);
+            tripDB.addPrice(payerId1, 2, 3.0);
+            double[] priceArray = tripDB.getPriceArray(payerId1);
+            double[] input = new double[] { 1.0, 2.0, 3.0 };
+            CollectionAssert.AreEqual(input, priceArray);
+        }
+
+        [TestMethod()]
+        public void getPayerArrayTest()
+        {
+            TripDB tripDB = new TripDB();
+            int tripID = tripDB.addTrip(3);
+            int payerId1 = tripDB.addPayer(tripID, 0, 1);
+            int payerId2 = tripDB.addPayer(tripID, 1, 1);
+            int payerId3 = tripDB.addPayer(tripID, 2, 1);
+            int[] input = new int[] { payerId1, payerId2, payerId3};
+            int[] payers= tripDB.getPayerArray(tripID);
+            CollectionAssert.AreEqual(input, payers);
+        }
+
+        [TestMethod()]
+        public void getTripAverageTest()
+        {
+            TripDB tripDB = new TripDB();
+            int tripID = tripDB.addTrip(3);
+            int payerId1 = tripDB.addPayer(tripID, 0, 1);
+            int payerId2 = tripDB.addPayer(tripID, 1, 1);
+            int payerId3 = tripDB.addPayer(tripID, 2, 1);
+            tripDB.addPrice(payerId1, 0, 5.0);
+            tripDB.addPrice(payerId2, 0, 10.0);
+            tripDB.addPrice(payerId3, 0, 15.0);
+            double average = tripDB.getTripAverage(tripID);
+            Assert.AreEqual(average, 10.0);
+        }
+
+        [TestMethod()]
+        public void getPayersRemainTest()
+        {
+            TripDB tripDB = new TripDB();
+            int tripID = tripDB.addTrip(3);
+            int payerId1 = tripDB.addPayer(tripID, 0, 1);
+            int payerId2 = tripDB.addPayer(tripID, 1, 1);
+            int payerId3 = tripDB.addPayer(tripID, 2, 1);
+            tripDB.addPrice(payerId1, 0, 5.0);
+            tripDB.addPrice(payerId2, 0, 10.0);
+            tripDB.addPrice(payerId3, 0, 15.0);
+            double[] remains = tripDB.getPayersRemain(tripID);
+            double[] input = new double[] {5.0, 0, -5.0};
+            CollectionAssert.AreEqual(input, remains);
+        }
+    }
+}
